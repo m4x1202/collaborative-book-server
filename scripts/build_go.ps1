0cce5fa724ff738dfaa2683e78241099c940295b
Max Walther <maxiwalther@mac.com>
2020-09-16 01:06:46 +0200

    Switch build scripts to new structure

--- scripts/build_go.ps1
+++ scripts/build_go.ps1
@@ -1,6 +1,9 @@
 $env:GOOS = "linux"
 $env:CGO_ENABLED = "0"
 $env:GOARCH = "amd64"
-go build -o bin\main server.go
+
+go test .\...
+
+go build -ldflags="-s -w" -o bin\main cmd\lambda\main.go
 build-lambda-zip.exe -output bin\main.zip bin\main
 aws lambda update-function-code --function-name collaborative-book --zip-file fileb://bin\main.zip
--- scripts/build_go.sh
+++ scripts/build_go.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-go build -o bin/main server.go
+export GOOS=linux
+export GOARCH=amd64
+
+go test ./...
+
+go build -ldflags="-s -w" -o bin/main cmd/lambda/main.go
 zip --junk-paths bin/main.zip bin/main
 aws lambda update-function-code --function-name collaborative-book --zip-file fileb://bin/main.zip
