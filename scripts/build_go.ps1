366fda650ab1c1ee57cd67bca79bdfd3075d3a87
Max Walther <maxiwalther@mac.com>
2020-11-29 20:48:16 +0100

    Fix running tests with local vscode build task

--- scripts/build_go.ps1
+++ scripts/build_go.ps1
@@ -1,8 +1,8 @@
+go test ./...
+
 $env:GOOS = "linux"
 $env:CGO_ENABLED = "0"
 $env:GOARCH = "amd64"
-
-go test ./...
 
 go build -ldflags="-s -w -X 'github.com/m4x1202/collaborative-book.Version=0.1.0'" -o bin/main cmd/lambda/main.go
 build-lambda-zip.exe -output bin/main.zip bin/main
