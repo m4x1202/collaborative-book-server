197ad1045232f9316b356840323d9edb2e46c983
Max Walther <maxiwalther@mac.com>
2024-03-08 23:46:42 +0100

    Add lambda.norpc flag to build scripts

--- scripts/build_go.ps1
+++ scripts/build_go.ps1
@@ -4,6 +4,6 @@ $env:GOOS = "linux"
 $env:CGO_ENABLED = "0"
 $env:GOARCH = "amd64"
 
-go build -ldflags="-s -w -X 'github.com/m4x1202/collaborative-book/internal/app/version.Version=0.1.1'" -o bin/main cmd/lambda/main.go
+go build -tags lambda.norpc -ldflags="-s -w -X 'github.com/m4x1202/collaborative-book/internal/app/version.Version=0.1.1'" -o bin/main cmd/lambda/main.go
 build-lambda-zip.exe -output bin/main.zip bin/main
 aws lambda update-function-code --function-name collaborative-book --zip-file fileb://bin/main.zip
